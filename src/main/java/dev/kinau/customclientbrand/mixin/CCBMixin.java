c0d45f4b913c7254b1a215d0482d64c532f75c97
TheErrorExe <161362055+TheErrorExe@users.noreply.github.com>
2025-04-14 14:57:25 +0000

    Update CCBMixin.java

--- src/main/java/dev/kinau/customclientbrand/mixin/CCBMixin.java
+++ src/main/java/dev/kinau/customclientbrand/mixin/CCBMixin.java
@@ -1,7 +1,5 @@
-package dev.kinau.customclientbrand.mixin;
+package dev.theerrorexe.glitchbrand.mixin;
 
-import dev.kinau.customclientbrand.config.CCBConfig;
-import me.shedaniel.autoconfig.AutoConfig;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.protocol.common.custom.BrandPayload;
 import org.spongepowered.asm.mixin.Mixin;
@@ -12,11 +10,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(BrandPayload.class)
 public class CCBMixin {
 
-	@Inject(method = "write(Lnet/minecraft/network/FriendlyByteBuf;)V", at = @At(value = "HEAD"), cancellable = true)
-	public void changeClientBrand(FriendlyByteBuf buf, CallbackInfo ci) {
-		String customBrand = AutoConfig.getConfigHolder(CCBConfig.class).get().customBrand();
-		if (customBrand == null || customBrand.isEmpty()) return;
-		buf.writeUtf(customBrand);
-		ci.cancel();
-	}
+    @Inject(method = "write(Lnet/minecraft/network/FriendlyByteBuf;)V", at = @At(value = "HEAD"), cancellable = true)
+    public void changeClientBrand(FriendlyByteBuf buf, CallbackInfo ci) {
+        buf.writeUtf("Glitchclient");
+        ci.cancel();
+    }
 }
