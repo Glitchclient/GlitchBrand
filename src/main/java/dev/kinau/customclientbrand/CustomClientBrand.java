0aa1fa7c79a219e4abfb343fb6e6e6a37792b143
TheErrorExe <161362055+TheErrorExe@users.noreply.github.com>
2025-04-14 14:56:44 +0000

    edit

--- src/main/java/dev/kinau/customclientbrand/CustomClientBrand.java
+++ src/main/java/dev/kinau/customclientbrand/CustomClientBrand.java
@@ -1,17 +1,13 @@
-package dev.kinau.customclientbrand;
+package dev.theerrorexe.glitchbrand;
 
-import dev.kinau.customclientbrand.config.CCBConfig;
-import me.shedaniel.autoconfig.AutoConfig;
-import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class CustomClientBrand implements ModInitializer {
-	public static final Logger LOGGER = LoggerFactory.getLogger("customclientbrand");
+    public static final Logger LOGGER = LoggerFactory.getLogger("glitchbrand");
 
-	@Override
-	public void onInitialize() {
-		AutoConfig.register(CCBConfig.class, GsonConfigSerializer::new);
-	}
+    @Override
+    public void onInitialize() {
+    }
 }
