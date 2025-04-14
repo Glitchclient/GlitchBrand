30ed741e27b78648adefe3dd1ca0ab46a94dbb09
TheErrorExe <161362055+TheErrorExe@users.noreply.github.com>
2025-04-14 15:29:11 +0000

    Update and rename CustomClientBrand.java to GlitchBrand.java

--- src/main/java/dev/theerrorexe/glitchbrand/GlitchBrand.java
+++ src/main/java/dev/theerrorexe/glitchbrand/GlitchBrand.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class CustomClientBrand implements ModInitializer {
+public class GlitchBrand implements ModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger("glitchbrand");
 
     @Override
